a3a51a3aa840416433bdb658916baa551c9f0061
vinhvi <vithevinh123@gmail.com>
2023-05-13 11:47:05 +0700

    fix template

--- src/main/java/com/web/techNet/secityConfig/SecurityConfig.java
+++ src/main/java/com/web/techNet/secityConfig/SecurityConfig.java
@@ -78,10 +78,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
-    // Cho phep truy xuat Rest API ben ngoai Domain khac
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
-    }
-
 }
